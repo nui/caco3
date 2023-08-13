4e7e382850ce622146055389d48ba9002e25dc23
Nui Narongwet <narongwet.m@gmail.com>
2023-08-13 23:43:45 +0700

    Use git command to fix build problem

--- caco3-build-info/src/lib.rs
+++ caco3-build-info/src/lib.rs
@@ -5,7 +5,6 @@ use std::str::FromStr;
 use std::time::{SystemTime, UNIX_EPOCH};
 
 use anyhow::{bail, Context, Result};
-use git2::{Repository, StatusOptions, StatusShow};
 use serde::{Deserialize, Serialize};
 
 #[derive(Clone, Debug, Deserialize, Serialize, Eq, PartialEq)]
@@ -13,72 +12,32 @@ pub struct BuildInfo {
     pub build_profile: String,
     pub build_target: String,
     pub epoch_seconds: u64,
-    pub git: Option<Git>,
+    pub git_sha: Option<GitSha>,
     pub rustc_version: String,
 }
 
 #[derive(Clone, Debug, Deserialize, Serialize, Eq, PartialEq)]
-pub struct Git {
-    commit_id: CommitId,
-    dirty: bool,
-}
-
-#[derive(Clone, Debug, Deserialize, Serialize, Eq, PartialEq)]
 #[serde(transparent)]
-pub struct CommitId(String);
+pub struct GitSha(String);
 
 impl BuildInfo {
-    pub fn get() -> Result<Self> {
+    pub fn from_build_script() -> Result<Self> {
         let build_target = std::env::var("TARGET")?;
         let build_profile = std::env::var("PROFILE")?;
         let epoch_seconds = get_epoch_seconds()?;
-        let git = Git::get().ok();
+        let git_sha = GitSha::from_cmd();
         let rustc_version = get_rustc_version()?;
         Ok(Self {
             build_target,
             build_profile,
             epoch_seconds,
-            git,
+            git_sha,
             rustc_version,
         })
     }
 }
 
-impl Git {
-    pub fn get() -> Result<Self> {
-        let repo = Repository::open_from_env()?;
-        let commit_id = get_commit_id(&repo)?;
-        let dirty = get_dirty(&repo)?;
-        Ok(Git { commit_id, dirty })
-    }
-
-    /// Return a string which represent commit id and dirty status
-    pub fn to_commit_and_dirty(&self) -> String {
-        let mut output = String::with_capacity(self.commit_id.as_str().len() + 1);
-        output.push_str(self.commit_id.as_str());
-        if self.dirty {
-            output.push('*');
-        }
-        output
-    }
-
-    pub fn from_commit_id_and_dirty(id: &str, dirty: bool) -> Option<Self> {
-        is_valid_id(id).then(|| Self {
-            commit_id: CommitId(id.to_string()),
-            dirty,
-        })
-    }
-
-    pub fn commit_id(&self) -> &CommitId {
-        &self.commit_id
-    }
-
-    pub fn dirty(&self) -> bool {
-        self.dirty
-    }
-}
-
-impl CommitId {
+impl GitSha {
     pub fn as_str(&self) -> &str {
         &self.0
     }
@@ -92,9 +51,24 @@ impl CommitId {
             .unwrap_or(id.len());
         &id[..index]
     }
+
+    pub fn from_cmd() -> Option<Self> {
+        let output = Command::new("git")
+            .arg("rev-parse")
+            .arg("HEAD")
+            .output()
+            .ok()?;
+        let mut stdout = String::from_utf8(output.stdout).ok()?;
+        stdout.truncate(stdout.trim().len());
+        if is_valid_id(&stdout) {
+            Some(GitSha(stdout))
+        } else {
+            None
+        }
+    }
 }
 
-impl FromStr for CommitId {
+impl FromStr for GitSha {
     type Err = ();
 
     fn from_str(s: &str) -> std::result::Result<Self, Self::Err> {
@@ -118,7 +92,9 @@ fn is_valid_id(id: &str) -> bool {
 }
 
 fn get_rustc_version() -> Result<String> {
-    let output = Command::new("rustc")
+    let rustc = std::env::var("RUSTC_WRAPPER").ok();
+    let rustc = rustc.as_deref().unwrap_or("rustc");
+    let output = Command::new(rustc)
         .arg("--version")
         .output()
         .context("get rustc version")?;
@@ -129,26 +105,6 @@ fn get_rustc_version() -> Result<String> {
         .context("version output to utf8")?
         .trim();
     Ok(version.into())
-}
-
-fn get_commit_id(repo: &Repository) -> Result<CommitId> {
-    let revspec = repo.revparse("HEAD")?;
-    let id = revspec
-        .from()
-        .context("HEAD commit should have 'from' range")?
-        .id();
-    Ok(CommitId(id.to_string()))
-}
-
-fn get_dirty(repo: &Repository) -> Result<bool> {
-    let mut options = StatusOptions::new();
-    options
-        .exclude_submodules(true)
-        .include_untracked(false)
-        .show(StatusShow::IndexAndWorkdir);
-    let statuses = repo.statuses(Some(&mut options))?;
-    let clean = statuses.is_empty();
-    Ok(!clean)
 }
 
 fn get_epoch_seconds() -> Result<u64> {
@@ -165,29 +121,8 @@ macro_rules! rustc_env {
 #[cfg(test)]
 mod tests {
     use super::*;
-
     #[test]
-    fn git() {
-        let commit_id = "12345678";
-        let clean_git = Git {
-            commit_id: CommitId(commit_id.to_string()),
-            dirty: false,
-        };
-        let actual = clean_git.to_commit_and_dirty();
-        assert_eq!(actual, commit_id);
-
-        let dirty_git = Git {
-            commit_id: CommitId(commit_id.to_string()),
-            dirty: true,
-        };
-        let actual = dirty_git.to_commit_and_dirty();
-        assert_eq!(actual, format!("{commit_id}*"));
-    }
-
-    #[test]
-    fn git_from_commit_id() {
-        assert!(Git::from_commit_id_and_dirty("123456", false).is_none());
-        assert!(Git::from_commit_id_and_dirty("1234567", false).is_some());
-        assert!(Git::from_commit_id_and_dirty("1234567-", false).is_none());
+    fn test_valid_id() {
+        assert!(is_valid_id("1460ba33e88a6caff86948da489be527fa442a9a"));
     }
 }
